ba3a38410f646c1d6e3580ca448acff51d1c31aa
thenuka32 <156032037+thenuka32@users.noreply.github.com>
2024-01-10 03:27:12 +0000

    added name and age inputs and added option 4

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Hello world")
+name = input("What is your name?: ")
+age = input("What is your age?: ")
+print("Hello " + name + "What would you like to talk about?")
+print("1.placeHolder \n2.placeHolder \n3.placeHolder \n4.exit")
+choice = int(input("Enter your choice: "))
+if choice == 4:
+  print("Goodbye")
+
